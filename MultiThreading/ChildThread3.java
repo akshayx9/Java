2b1cbefc07d23338029af15ee7800151444d1d8e
Akshay <akshaysinghx9@gmail.com>
2023-03-26 07:39:35 +0530

    seventh commit

---
+++ MultiThreading/ChildThread3.java
@@ -0,0 +1,14 @@
+package MultiThreading;
+
+public class ChildThread3 extends Thread {
+    int total=0;
+    public void run() {
+        synchronized(this){
+            System.out.println("Child Thread is starting...");
+            for(int i=1; i<=10; i++){
+            total+=i;
+            }
+            System.out.println("Child Thread giving notification");
+        }        
+    }
+}
